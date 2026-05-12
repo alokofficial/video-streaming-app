fb9dcaeccf9eb9a84605ecf0fc24710c9085b02a
Alok Kumar <aloknh31@gmail.com>
2026-05-12 12:41:05 +0530

    Change header text from 'Streaming Platform' to 'Contents'

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -30,7 +30,7 @@ export default function Home() {
       <Navbar />
 
       <div className="p-6">
-        <h1 className="text-4xl font-bold mb-8">Streaming Platform</h1>
+        <h1 className="text-4xl font-bold mb-8">Contents</h1>
 
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {videos.map((video) => (
